84256a85f6d34a187c546e27829c3627a9ce1bd5
beefykenny <hoftkenny@gmail.com>
2022-03-06 20:28:13 -0300

    changed error message

--- users.js
+++ users.js
@@ -8,7 +8,13 @@ const addUser = ({ id, name, room }) => {
     (user) => user.room === room && user.name === name
   );
 
-  if (existingUser) return { error: "Username is taken" };
+  if (existingUser) {
+    return {
+      error: {
+        message: "This name is taken, Try a different one.",
+      },
+    };
+  }
 
   const user = { id, name, room };
   users.push(user);
